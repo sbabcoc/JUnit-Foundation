b96c955dd57de2db0b539a3e52c97d0e0b177e22
Scott Babcock <scoba@hotmail.com>
2017-10-15 19:46:25 -0700

    Add JavaDoc; remove pointless overrides

--- src/test/java/com/nordstrom/automation/junit/RunListenerAdapter.java
+++ src/test/java/com/nordstrom/automation/junit/RunListenerAdapter.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.runner.Description;
-import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 
@@ -18,23 +17,8 @@ public class RunListenerAdapter extends RunListener {
     private List<Description> m_passedTests = Collections.synchronizedList(new ArrayList<>());
                 
     /**
-     * Called before any tests have been run.
-     * @param description describes the tests to be run
-     */
-    @Override
-    public void testRunStarted(Description description) throws Exception {
-    }
-    
-    /**
-     * Called when all tests have finished
-     * @param result the summary of the test run, including all the tests that failed
-     */
-    @Override
-    public void testRunFinished(Result result) throws Exception {
-    }
-    
-    /**
      * Called when an atomic test is about to be started.
+     * 
      * @param description the description of the test that is about to be run 
      * (generally a class and method name)
      */
@@ -43,16 +27,9 @@ public class RunListenerAdapter extends RunListener {
         m_allTestMethods.add(description);
     }
 
-    /**
-     * Called when an atomic test has finished, whether the test succeeds or fails.
-     * @param description the description of the test that just ran
-     */
-    @Override
-    public void testFinished(Description description) throws Exception {
-    }
-
     /** 
      * Called when an atomic test fails.
+     * 
      * @param failure describes the test that failed and the exception that was thrown
      */
     @Override
@@ -84,10 +61,20 @@ public class RunListenerAdapter extends RunListener {
         m_ignoredTests.add(description);
     }
     
+    /**
+     * Get list of all tests that were run.
+     * 
+     * @return list of all tests
+     */
     public List<Description> getAllTestMethods() {
         return m_allTestMethods;
     }
     
+    /**
+     * Get list of passed tests.
+     * 
+     * @return list of passed tests
+     */
     public List<Description> getPassedTests() {
         m_passedTests.clear();
         m_passedTests.addAll(m_allTestMethods);
@@ -97,14 +84,29 @@ public class RunListenerAdapter extends RunListener {
         return m_passedTests;
     }
     
+    /**
+     * Get list of failed tests.
+     * 
+     * @return list of failed tests
+     */
     public List<Description> getFailedTests() {
         return m_failedTests;
     }
     
+    /**
+     * Get list of failed assumptions.
+     * 
+     * @return list of failed assumptions
+     */
     public List<Description> getFailedAssumptions() {
         return m_failedAssumptions;
     }
     
+    /**
+     * Get list of ignored tests.
+     * 
+     * @return list of ignored tests
+     */
     public List<Description> getIgnoredTests() {
         return m_ignoredTests;
     }
