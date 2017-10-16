ff02d42f936900db538ea0abc8a03a2e16417b9f
Scott Babcock <scoba@hotmail.com>
2017-10-15 19:16:32 -0700

    Add JavaDoc

--- src/main/java/com/nordstrom/automation/junit/JUnitArtifactParams.java
+++ src/main/java/com/nordstrom/automation/junit/JUnitArtifactParams.java
@@ -1,10 +1,14 @@
 package com.nordstrom.automation.junit;
 
+/**
+ * This interface enables implementers to provide method-related parameters to the artifact capture framework.
+ */
 public interface JUnitArtifactParams {
     
     /**
+     * Get the parameters associated with this test class instance.
      * 
-     * @return
+     * @return array of test class parameters
      */
     default Object[] getParameters() {
         return new Object[0];
