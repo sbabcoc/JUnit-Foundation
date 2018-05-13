8002d25dd325ba622a6612cc09bdcc32c6264362
Scott Babcock <scoba@hotmail.com>
2018-05-13 14:04:40 -0700

    Add a couple of code comments

--- src/main/java/com/nordstrom/automation/junit/HookInstallingRunner.java
+++ src/main/java/com/nordstrom/automation/junit/HookInstallingRunner.java
@@ -244,7 +244,9 @@ public final class HookInstallingRunner extends BlockJUnit4ClassRunner {
         // determine if retry is disabled for the class that declares this method
         NoRetry noRetryOnClass = method.getDeclaringClass().getAnnotation(NoRetry.class);
         
+        // if method isn't ignored or excluded from being retried
         if (!isIgnored(method) && (noRetryOnMethod == null) && (noRetryOnClass == null)) {
+            // get configured maximum retry count
             maxRetry = config.getInteger(JUnitSettings.MAX_RETRY.key(), Integer.valueOf(0));
         }
         
