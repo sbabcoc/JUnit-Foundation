7cd25849ec66343c4631870c1a5c3e9de85d49ae
Scott Babcock <scoba@hotmail.com>
2018-12-08 00:29:54 -0800

    Remove debug code

--- src/test/java/com/nordstrom/automation/junit/AutomaticRetryFailing.java
+++ src/test/java/com/nordstrom/automation/junit/AutomaticRetryFailing.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 
 public class AutomaticRetryFailing {
     
-    public AutomaticRetryFailing() {
-        
-    }
-    
     @Test
     public void testFailed() {
         System.out.println("testFailed");
