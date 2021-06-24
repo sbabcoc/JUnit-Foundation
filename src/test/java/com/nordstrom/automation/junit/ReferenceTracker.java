46529bbfed4e8518810ebd45535ef73b75653947
Scott Babcock <scoba@hotmail.com>
2021-06-24 11:52:26 -0700

    Add code comments

--- src/test/java/com/nordstrom/automation/junit/ReferenceTracker.java
+++ src/test/java/com/nordstrom/automation/junit/ReferenceTracker.java
@@ -11,8 +11,11 @@ public class ReferenceTracker implements RunnerWatcher {
 
     @Override
     public void runFinished(Object runner) {
+        // get a reference to the ReferenceChecker run listener
         Optional<ReferenceChecker> checker = LifecycleHooks.getAttachedListener(ReferenceChecker.class);
+        // if listener is present
         if (checker.isPresent()) {
+            // invoke "run finished" handler
             checker.get().runFinished(runner);
         }
     }
